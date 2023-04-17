3d5a9ec895769fe7ca60519c89b2149dc12dba8b
maaaz5 <mohamade.amine12345@gmail.com>
2023-04-17 07:34:04 +0000

    Transactions FrontEnd Page added

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -59,10 +59,16 @@ export const getTransactions = async (req, res) => {
       .limit(pageSize);
 
     const total = await Transaction.countDocuments({
-      name: { $regex: search, $options: "i" },
+      $or: [
+        { cost: { $regex: new RegExp(search, "i") } },
+        { userId: { $regex: new RegExp(search, "i") } },
+      ],
     });
 
-    res.status(200).json({ transactions, total });
+    res.status(200).json({
+      transactions,
+      total,
+    });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
